210b0a54c9bd278ab33bcda4a896cf2497304ad3
dbouwyn <dimitri.bouwyn@gmail.com>
2015-10-04 16:45:17 +0200

    corrections

--- modules/simple_puppet/manifests/install.pp
+++ modules/simple_puppet/manifests/install.pp
@@ -1,14 +1,12 @@
 class simple_puppet::install inherits conf::install::wget {
-
   Package["wget"] {
-    ensure => latest,
-  }
+    ensure => latest, }
   Package["wget"] -> Exec["/usr/bin/wget https://apt.puppetlabs.com/puppetlabs-release-${lsbdistcodename}.deb"]
 
   # monthly update
   cron { "force apt.puppetlabs update":
     command => "/usr/bin/dpkg -r puppetlabs-release ; /bin/rm /tmp/puppetlabs-release-${lsbdistcodename}.deb",
-    #ensure  => absent,
+    # ensure  => absent,
     user    => root,
     special => 'monthly',
     require => Service["cron"],
@@ -28,21 +26,21 @@ class simple_puppet::install inherits conf::install::wget {
     notify   => Exec["/usr/bin/apt-get update"],
   }
 
-  package { ["puppet","facter"]:
-    ensure => latest,
+  package { ["puppet", "facter"]:
+    ensure  => latest,
     require => Exec["/usr/bin/dpkg -i puppetlabs-release-${lsbdistcodename}.deb", "/usr/bin/apt-get update"],
   }
-  
+
   package { "puppetmaster":
     ensure  => purged,
     require => Package["puppet"],
   }
-  
+
   file { "/etc/puppet":
     ensure  => directory,
     require => Package["puppet"],
   }
-  
 
+  file { "/etc/puppet/modules": ensure => directory, }
 }
 
