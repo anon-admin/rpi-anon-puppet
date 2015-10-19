e21ed3093f823ff86c10526c4b1fb6bf3b0474d2
anon-admin <dimitri.83.fr@gmail.com>
2015-10-19 21:40:10 +0200

    correction

--- modules/userids/manifests/init.pp
+++ modules/userids/manifests/init.pp
@@ -21,8 +21,8 @@ class userids ($admin_pwd = $userids::params::admin_pwd, $admin_pwd_cr = $userid
   $anonymous_user = "anonymous"
 
   contain userids::conf::proxygroup
-  $squid_id = userids::conf::proxygroup::proxy_id
-  $squid_user = userids::conf::proxygroup::proxy_group
+  $squid_id = $userids::conf::proxygroup::proxy_id
+  $squid_user = $userids::conf::proxygroup::proxy_group
 
   $pdnsd_id = 108
   $pdnsd_user = pdnsd
