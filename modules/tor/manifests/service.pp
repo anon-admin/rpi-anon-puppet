547df07b0fa93620844ed13faf3eba5bbf6b7e12
dbouwyn <dimitri.bouwyn@gmail.com>
2015-11-05 21:46:52 +0100

    correction

--- modules/tor/manifests/service.pp
+++ modules/tor/manifests/service.pp
@@ -1,7 +1,13 @@
 class tor::service($tor_user = $tor::tor_user) inherits tor {
+  file { "/var/run/tor":
+    ensure => directory,
+    owner  => $tor_user,
+    group  => $tor_user,
+  }
+
   service { "tor":
     ensure  => running,
     enable  => true,
-    require => [File["/etc/default/tor", "/etc/tor/torrc"], Exec["sticky on /var/log/tor"], User["${tor_user}"]],
+    require => [File["/etc/default/tor", "/etc/tor/torrc", "/var/run/tor"], Exec["sticky on /var/log/tor"], User["${tor_user}"]],
   }
 }
