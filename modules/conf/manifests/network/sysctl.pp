e4f9246f6084695569749f7213f84092d8173253
anon-admin <dimitri.83.fr@gmail.com>
2015-11-07 17:03:12 +0100

    correction

--- modules/conf/manifests/network/sysctl.pp
+++ modules/conf/manifests/network/sysctl.pp
@@ -1,6 +1,6 @@
 class conf::network::sysctl($source, $interfaces) {
   file { "/etc/sysctl.d/${source}":
-    content => template("/conf/${source}.erb"),
+    content => template("conf/${source}.erb"),
   }
 
 }
