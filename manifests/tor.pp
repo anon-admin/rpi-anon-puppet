f6d93323f22964687b722570d9ca275d4cff186e
trusty <dimitri.bouwyn@gmail.com>
2016-05-12 14:10:17 +0000

    simplification tor pour premiers tests

--- manifests/tor.pp
+++ manifests/tor.pp
@@ -102,17 +102,18 @@ node default {
   }
   
   
-  include source_interfaces
-  include source_resolv
+  #include source_interfaces
+  #include source_resolv
 
   include rsyslog
+  include monit
 
-  include consts
-  class { 'conf::apt_proxy': routeur => $consts::routeur_ip, }
+  #include consts
+  #class { 'conf::apt_proxy': routeur => $consts::routeur_ip, }
   
-  include iptables
+  #include iptables
   
-  include tor
-  include pdnsd
+  #include tor
+  #include pdnsd
 
 }
