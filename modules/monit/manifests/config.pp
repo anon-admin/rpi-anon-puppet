6b4938dd44250ed367ce0a708b20ba7fc8c58c97
trusty <dimitri.bouwyn@gmail.com>
2016-05-12 19:02:30 +0000

    ajout monitrc

--- modules/monit/manifests/config.pp
+++ modules/monit/manifests/config.pp
@@ -1,12 +1,13 @@
 class monit::config inherits monit::minimal::config {
   contain monit::install
-  
-   file { "/etc/monit/monitrc":
-      owner => root,
-      group => root,
-      mode => 400,
-      notify => Service["monit"],
-      require => [ Package["monit"], File["/etc/monit/monitrc.d"] ],
-    }
+
+  file { "/etc/monit/monitrc":
+    owner   => root,
+    group   => root,
+    mode    => 400,
+    notify  => Service["monit"],
+    source  => "puppet:///modules/monit/monitrc",
+    require => [Package["monit"], File["/etc/monit/monitrc.d"]],
+  }
 
 }
