6b11864ae4481601aee5f78163cc844f9fb62e13
trusty <dimitri.bouwyn@gmail.com>
2016-05-13 10:12:09 +0000

    suppressionde service, boulette

--- modules/tor/manifests/service.pp
+++ modules/tor/manifests/service.pp
@@ -5,15 +5,6 @@ class tor::service($tor_user = $tor::tor_user) inherits tor {
     group  => $tor_user,
   }
 
-  file { "/lib/systemd/system/tor.service":
-    owner  => $tor_user,
-    group  => $tor_user,
-    mode   => 644,
-    source => "puppet:///modules/tor/tor.service",    
-  }
-  User["${tor_user}"] -> File["/lib/systemd/system/tor.service"] -> Service["tor"]
-  File["/lib/systemd/system/tor.service"] ~> Exec["/bin/systemctl daemon-reload"]
-
   service { "tor":
     ensure  => running,
     enable  => true,
