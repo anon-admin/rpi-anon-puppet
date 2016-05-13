23dc8a7636eaf0b705f4c02a77fac41974b66132
trusty <dimitri.bouwyn@gmail.com>
2016-05-13 09:20:51 +0000

    ajout du fichier de service pour tor

--- modules/conf/manifests/config/systemd.pp
+++ modules/conf/manifests/config/systemd.pp
@@ -2,4 +2,11 @@ class conf::config::systemd inherits conf::install::systemd {
   Package["systemd"] {
     ensure => latest, }
 
+  exec { "/bin/systemctl daemon-reload":
+    cwd         => "/lib/systemd",
+    refreshonly => true,
+  }
+  Package["systemd"] -> Exec["/bin/systemctl daemon-reload"]
+  Exec["/bin/systemctl daemon-reload"] -> Service<| |> 
+
 }
