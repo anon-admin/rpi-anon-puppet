56126079953282d30e3005b03c2393c627c299dc
dbouwyn <dimitri.bouwyn@gmail.com>
2016-09-06 21:14:44 +0200

    passage a hiera, suppression de params

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -11,11 +11,11 @@
 # Sample Usage:
 #
 class ntp (
-  $ntp_hwsync     = $ntp::params::ntp_hwsync,
-  $ntp_hw_stratum = $ntp::params::ntp_hw_stratum,
-  $ntp_interface  = $ntp::params::ntp_interface,
-  $ntp_options    = $ntp::params::ntp_options,
-  $ntp_servers    = $ntp::params::ntp_servers) inherits ntp::params {
+  $ntp_hwsync,
+  $ntp_hw_stratum = undef,
+  $ntp_interface  = undef,
+  $ntp_options    = undef,
+  $ntp_servers) {
   contain ntp::install
   contain ntp::config
   contain ntp::service
