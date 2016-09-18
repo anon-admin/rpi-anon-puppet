1afe089355c95a572bdd6cd34cb924e0d2f2c8fb
dbouwyn <dimitri.bouwyn@gmail.com>
2016-09-18 18:23:42 +0200

    simple_puppet en hiera

--- modules/simple_puppet/manifests/init.pp
+++ modules/simple_puppet/manifests/init.pp
@@ -13,10 +13,10 @@
 class simple_puppet(
   $puppetadmin_user = $simple_puppet::params::puppetadmin_user,
   $puppetadmin_id = $simple_puppet::params::puppetadmin_id,
-  $puppetadmin_shell = $simple_puppet::params::puppetadmin_shell,
+  $puppetadmin_shell,
   $reports_dir = $simple_puppet::params::reports_dir,
-  $clientbucket_dir = $simple_puppet::params::clientbucket_dir,
-  $manifest = $simple_puppet::params::manifest) inherits simple_puppet::params {
+  $clientbucket_dir,
+  $manifest) inherits simple_puppet::params {
 
 }
 
