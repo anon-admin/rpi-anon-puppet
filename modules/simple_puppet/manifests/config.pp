ae5eb4c7112183c763ec4eb24032e7f2f1f5b4e5
anon-admin <anon-admin@githut.com>
2016-12-05 18:49:28 +0100

    correction hiera

--- modules/simple_puppet/manifests/config.pp
+++ modules/simple_puppet/manifests/config.pp
@@ -21,12 +21,6 @@ class simple_puppet::config inherits simple_puppet::install {
   File["${simple_puppet::conf_root_dir}/modules","${simple_puppet::conf_root_dir}/${simple_puppet::hiera_dir}"] {
     ensure => directory, }
 
-  $passwd_hiera_file = "${simple_puppet::hiera_dir}/passwords.yaml"
-
-  include stdlib
-  file_line { "gitignore ${passwd_hiera_file}":
-    path => "${simple_puppet::conf_root_dir}/.gitignore",
-    line => "${passwd_hiera_file}",
-  }
+ contain simple_puppet::ignore::yaml
 
 }
