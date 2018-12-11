84cd3104972bb81268e05a8b7e9035c07182c6ec
dbouwyn <dbouwyn.thales@gmail.com>
2018-12-11 08:31:13 +0100

    passage de ntp en sous projet

--- modules/ntp/manifests/install.pp
+++
@@ -1,7 +0,0 @@
-class ntp::install inherits ntp {
-  package { "ntp":
-    ensure => latest,
-    require => File["/etc/apt/apt.conf.d/02periodic"],
-  }
-
-}
--- modules/ntp/manifests/monit.pp
+++
@@ -1,6 +0,0 @@
-class ntp::monit inherits monit::minimal::config {
-  
-  monit::fullfill_service{ "ntpd": 
-    module => "ntp",
-  }
-}
